1da07f379719652d320ffab1c5e1e3ee16b77861
Mayesha Tasnim <mayeshatasnim54@gmail.com>
2024-04-10 05:21:44 +0600

    Signup Added

---
+++ lib/view/login_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:sdp2/utils/global_colors.dart';
+import 'package:sdp2/view/signup_view.dart';
+import 'package:sdp2/view/widgets/global_textform.dart';
+
+class LoginView extends StatelessWidget {
+  LoginView({super.key});
+  final TextEditingController emailController=TextEditingController();
+  final TextEditingController passwordController=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Text(
+                    'Logo',
+                    style: TextStyle(
+                      color: GlobalColors.mainColor,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Text(
+                  'Login to your Account',
+                  style: TextStyle(
+                      color: GlobalColors.textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500
+                  ),
+                ),
+                const SizedBox(height: 15),
+                ///Email Input
+                GlobalTextForm(
+                  controller: emailController,
+                  text: 'Email',
+                  textInputType: TextInputType.emailAddress,
+                  obscure: false,
+                ),
+                const SizedBox(height: 10),
+                ///Password Input
+                GlobalTextForm(
+                  controller: passwordController,
+                  text: 'Password',
+                  textInputType: TextInputType.text,
+                  obscure: true,
+                ),
+                ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupView()));
+                }, child: Text('Sign up with'))
+
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/splash_view.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sdp2/utils/global_colors.dart';
+import 'package:sdp2/view/login_view.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
+      Get.to(LoginView());
+    });
+    return Scaffold(
+      backgroundColor: GlobalColors.mainColor,
+      body: const Center(
+        child: Text('Logo',style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 35
+        ),),
+      ),
+    );
+  }
+}
