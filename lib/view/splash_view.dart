28eb411e0bbb5955474ffe0dfc2a5e904effddfe
SadiaShitol <shitol3080@gmail.com>
2024-06-16 09:02:00 +0600

    Added 3 Onboarding pages and naviagted them from the beginning

--- lib/view/splash_view.dart
+++ lib/view/splash_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'dart:async';
 import 'package:sdp2/view/login_view.dart';
+import 'package:sdp2/view/onboarding/Onboarding.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -9,8 +10,8 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Schedule a navigation to LoginScreen() after 5 seconds
-    Timer(const Duration(seconds: 5), () {
-      Get.to(LoginView());
+    Timer(const Duration(seconds: 3), () {
+      Get.to(Onboarding());
     });
 
     return Scaffold(
